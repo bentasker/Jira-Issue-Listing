7176dd93a748d26cab5d69d31c6be05f2d1eef84
B Tasker <btasker@velocix.com>
2014-08-17 13:08:05 +0100

    Moved DB class and config to seperate files

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+/** JIRA Issue List Script - Configuration
+*
+* Simple script to generate an simple HTML listing of JIRA Issues from a Private JIRA Instance
+* Intended use is to allow indexing of JIRA issues by internal search engines such as Sphider (http://www.sphider.eu/)
+*
+* Documentation: http://www.bentasker.co.uk/documentation/development-programming/273-allowing-your-internal-search-engine-to-index-jira-issues
+*
+* @copyright (C) 2014 B Tasker (http://www.bentasker.co.uk). All rights reserved
+* @license GNU GPL V2 - See LICENSE
+*
+* @version 1.2
+*
+*/
+
+
+$conf->db = 'JiraDB';
+$conf->host = 'localhost';
+$conf->user = 'jiradbuser';
+$conf->password = '';
+$conf->dbprefix = '';
+
+$conf->scriptname='index.php';
+$conf->jiralocation = 'http://jira.example.com';
+$conf->SphiderUA = 'Sphider';
+$conf->SphiderIP = array('192.168.1.65/32','192.168.1.96'); // You can use CIDR or specify individual IPs
+
+
+$conf->debug = false; // Enabling this will prevent redirection to JIRA
